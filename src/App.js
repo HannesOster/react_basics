e8d68f180d33b065e39d5a50d027f28ae0566915
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-22 12:37:43 +0200

    try objecets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./styles.css";
 
+const object = { name: "johannes", age: 28, greeting: "<h1>Hello World</h1>" };
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return HelloWorld(object);
 }
 
-function HelloWorld() {
-  return <h1>Hello world</h1>;
+function HelloWorld(object) {
+  return (
+    <div>
+      object.greeting
+      <p>
+        I am {object.name} i am {object.age} years old
+      </p>
+      ;
+    </div>
+  );
 }
